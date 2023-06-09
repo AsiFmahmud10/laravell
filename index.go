ce380cad304e4ebba3b80fdfae88be9c821da573
asif <mhamudasif2@gmail.com>
2023-06-09 16:07:03 +0600

    asd

--- index.go
+++ index.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
+	//"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -47,18 +47,18 @@ func main(){
 	r := mux.NewRouter()
 	
 	PORT := os.Getenv("PORT")
-	HOST := os.Getenv("HOST")
-	USER := os.Getenv("USER")
-	PASSWORD := os.Getenv("PASSWORD")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_PORT := os.Getenv("DB_PORT")
 	
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",HOST,USER,PASSWORD ,DB_NAME,DB_PORT)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",DB_HOST,DB_USER,DB_PASSWORD ,DB_NAME,DB_PORT)
 	
     print(dsn);
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    Db = db;
+	//db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    //Db = db;
 	if err != nil{
 		print("Nowww")	
 	}
