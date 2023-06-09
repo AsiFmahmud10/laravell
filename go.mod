6c645798bbd7f8684d266c0a9f0af246aeab9e80
asif <mhamudasif2@gmail.com>
2023-06-09 06:06:42 +0600

    :/:

---
+++ go.mod
@@ -0,0 +1,15 @@
+module salt/m
+
+go 1.18
+
+require gorm.io/gorm v1.25.1
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/mysql v1.5.1
+)
---
+++ index.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"os"
+    "github.com/joho/godotenv"
+	"github.com/gorilla/mux"
+)
+
+func HomeHandler( w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("welcome"))
+}
+
+func main(){
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+	r := mux.NewRouter()
+	
+	PORT := os.Getenv("PORT")
+
+
+	r.HandleFunc("/", HomeHandler)
+	http.ListenAndServe(PORT,r)
+}
